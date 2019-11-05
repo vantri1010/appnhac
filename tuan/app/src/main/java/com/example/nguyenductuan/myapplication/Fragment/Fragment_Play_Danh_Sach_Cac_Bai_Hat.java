dfaa67efa1b7be3c20be2d72a3a43b12b69a75a4
TriVan <31728513+vantri1010@users.noreply.github.com>
2019-11-05 22:11:38 +0700

    unzip

---
+++ tuan/app/src/main/java/com/example/nguyenductuan/myapplication/Fragment/Fragment_Play_Danh_Sach_Cac_Bai_Hat.java
@@ -0,0 +1,40 @@
+package com.example.nguyenductuan.myapplication.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nguyenductuan.myapplication.Activity.PlayNhacActivity;
+import com.example.nguyenductuan.myapplication.Adapter.PlaynhacAdapter;
+import com.example.nguyenductuan.myapplication.R;
+
+/**
+ * Created by Nguyen Duc Tuan on 28-Oct-19.
+ */
+
+public class Fragment_Play_Danh_Sach_Cac_Bai_Hat extends android.support.v4.app.Fragment {
+
+    View view;
+    RecyclerView recyclerViewplaynhac;
+    PlaynhacAdapter playnhacAdapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_play_danh_sach_cac_bai_hat,container,false);
+        recyclerViewplaynhac = view.findViewById(R.id.recyclerviewPlaybaihat);
+
+        if(PlayNhacActivity.mangbaihat.size()>0)
+        {
+            playnhacAdapter = new PlaynhacAdapter(getActivity(),PlayNhacActivity.mangbaihat);
+            recyclerViewplaynhac.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerViewplaynhac.setAdapter(playnhacAdapter);
+        }
+        return view;
+    }
+}
